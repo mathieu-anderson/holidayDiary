7b97b4b918790bb79f05283709851260e60bc392
mathieu-anderson <mathieu.hj.anderson@gmail.com>
2017-01-07 12:45:04 +1300

    Additional express.router setup

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/index', function(req, res, next) {
-  res.render('index', { title: 'Holiday Diary' });
-});
-
-module.exports = router;
